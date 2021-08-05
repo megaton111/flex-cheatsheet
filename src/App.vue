529e70ba6ba1464fe19c8d62b3a93230e680de18
megaton111 <lkh890111@gmail.com>
2021-08-05 13:40:08 +0900

    head meta 구성

--- src/App.vue
+++ src/App.vue
@@ -26,7 +26,27 @@ export default {
     currentRouteName() {
       return this.$route.name;
     }
-  }
+  } ,
+  metaInfo: {
+    title: 'CSS Flex 레이아웃',
+    titleTemplate: '%s | BrickLab',
+    htmlAttrs: {
+    },
+    meta: [
+      { charset: 'utf-8'},
+      { name: 'description', content: 'CSS 레이아웃 Flex box 튜토리얼 공부하기', vmid: 'description'},
+      { name: 'keywords', content: "css,css3,flex,플렉스레이아웃,flexbox"},
+      { name: 'author', content: "BrickLab"},
+      {
+        property: 'og:title',
+        content: 'CSS Flex 레이아웃의 모든 것',
+        template: chunk => `${chunk} | BrickLab`,
+        vmid: 'og:title'
+      },
+      // mobile
+      { name: 'viewport', content: 'width=device-width, initial-scale=1'}
+    ]
+  } ,
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'; 
 import store from './store/index'; 
+import Meta from 'vue-meta';
 
 Vue.config.productionTip = false;
 
 import "@/assets/scss/_index.scss" ; 
+Vue.use(Meta)
 
 new Vue({
   router,
